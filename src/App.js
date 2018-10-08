c1f7dc0d0c778c88675161191181128ff759dcde
Thomas Gonzalez <thomasgonzalez.pro@gmail.com>
2018-10-08 17:41:20 +0200

    quete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
+import './App.css';
 
-import Timer from './timer.js'
-import './App.css'
+import Home from './components/Home';
+import History from './components/History';
 
 class App extends Component {
-  render () {
-      return (
-        <div className='App'>
-          <Timer startCount/>
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <NavLink exact to="/" activeClassName="selected"> <h3>Home</h3> </NavLink>
+            <NavLink to="/notre-histoire" activeClassName="selected"> <h3>Contact</h3> </NavLink>
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/notre-histoire" component={History} />
+            </Switch>
+          </div>
+        </BrowserRouter>
         </div>
-      )
-  }
+    );
+}
 }
 
-export default App
+export default App;
---
+++ src/components/History.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class History extends Component {
+    render() { 
+        return ( 
+            <div className="history">
+            <h1>Notre histoire</h1>
+            </div>
+         );
+    }
+}
+ 
+export default History ;
